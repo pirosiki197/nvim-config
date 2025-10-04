fd625f306dd3799f44d1c82f25e7e9292855882a
pirosiki197 <lingmukangtai825@gmail.com>
2025-10-05 08:52:11 +0900

    update

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -21,7 +21,7 @@ return {
     build = ":TSUpdate",
     config = function()
       require("nvim-treesitter.configs").setup({
-        ensure_installed = { "c", "lua", "vim", "vimdoc", "go" },
+        ensure_installed = { "c", "lua", "vim", "vimdoc", "go", "dockerfile" },
         highlight = { enable = true },
         indent = { enable = true },
       })
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,4 +1,18 @@
 return {
-  "neovim/nvim-lspconfig",
-  event = { "BufReadPre", "BufNewFile" },
+  {
+    "neovim/nvim-lspconfig",
+    event = { "BufReadPre", "BufNewFile" },
+  },
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      fomatters_by_ft = {
+        go = { "goimports" },
+      },
+      format_on_save = {
+        timeout_ms = 5000,
+        lsp_format = "fallback",
+      },
+    },
+  },
 }
