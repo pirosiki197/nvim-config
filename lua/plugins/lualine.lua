33cee7f951f4dd0a89562d02ae83dbc26396b08a
pirosiki197 <lingmukangtai825@gmail.com>
2025-10-07 14:33:56 +0900

    update

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -5,8 +5,10 @@ return {
     config = function()
       vim.opt.termguicolors = true
       require("bufferline").setup()
-      vim.keymap.set("n", "<Tab>", ":bnext<CR>")
-      vim.keymap.set("n", "<S-Tab>", ":bprev<CR>")
+      vim.keymap.set("n", "<A-.>", "<cmd>BufferLineMoveNext<CR>")
+      vim.keymap.set("n", "<A-,>", "<cmd>BufferLineMovePrev<CR>")
+      vim.keymap.set("n", "<Tab>", "<cmd>BufferLineCycleNext<CR>")
+      vim.keymap.set("n", "<S-Tab>", "<cmd>BufferLineCyclePrev<CR>")
     end,
   },
   {
