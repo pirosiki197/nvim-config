ce6614d81d87ebf8f9cbc88cc569f8fea9ac6d63
pirosiki197 <lingmukangtai825@gmail.com>
2025-10-22 17:21:25 +0900

    Add mini.move

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -4,6 +4,7 @@ return {
     require("mini.ai").setup()
     require("mini.surround").setup()
     require("mini.bufremove").setup()
+    require("mini.move").setup()
     vim.keymap.set("n", "<leader>bd", require("mini.bufremove").delete)
   end
 }
