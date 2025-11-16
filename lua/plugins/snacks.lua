59ef820da88ae7ae16a5f27f375765549054451a
pirosiki197 <lingmukangtai825@gmail.com>
2025-11-16 16:41:53 +0900

    Replace toggleterm with snacks.terminal

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -12,7 +12,8 @@ return {
   keys = {
     { "<c-n>",      function() Snacks.explorer.open() end,           desc = "Explorer" },
     { "<leader>gg", function() Snacks.lazygit() end,                 desc = "LazyGit" },
-    { "]]",         function() Snacks.words.jump(vim.v.count1) end,  desc = "Next Reference", mode = { "n", "t" } },
-    { "[[",         function() Snacks.words.jump(-vim.v.count1) end, desc = "Prev Reference", mode = { "n", "t" } },
+    { "]]",         function() Snacks.words.jump(vim.v.count1) end,  desc = "Next Reference",  mode = { "n", "t" } },
+    { "[[",         function() Snacks.words.jump(-vim.v.count1) end, desc = "Prev Reference",  mode = { "n", "t" } },
+    { "<c-_>",      function() Snacks.terminal() end,                desc = "Toggle Terminal", mode = { "n", "t" } },
   }
 }
--- lua/plugins/terminal.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "akinsho/toggleterm.nvim",
-  config = function()
-    require("toggleterm").setup({
-      open_mapping = { "<C-\\>", "<c-¥>" },
-      direction = "float",
-    })
-  end
-}
