7e255d749b55293db78d87aaa94fea18abe0b321
pirosiki197 <lingmukangtai825@gmail.com>
2025-12-01 17:59:10 +0900

    Replace telescope with Snacks.picker

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -8,13 +8,24 @@ return {
       enabled = true,
     },
     indent = {},
+    picker = { enabled = true },
     words = { enabled = true },
   },
   keys = {
-    { "<c-n>",      function() Snacks.explorer.open() end,           desc = "Explorer" },
-    { "<leader>gg", function() Snacks.lazygit() end,                 desc = "LazyGit" },
-    { "]]",         function() Snacks.words.jump(vim.v.count1) end,  desc = "Next Reference",  mode = { "n", "t" } },
-    { "[[",         function() Snacks.words.jump(-vim.v.count1) end, desc = "Prev Reference",  mode = { "n", "t" } },
-    { "<c-_>",      function() Snacks.terminal() end,                desc = "Toggle Terminal", mode = { "n", "t" } },
+    { "<c-n>",      function() Snacks.explorer.open() end,              desc = "Explorer" },
+    { "<leader>gg", function() Snacks.lazygit() end,                    desc = "LazyGit" },
+    { "]]",         function() Snacks.words.jump(vim.v.count1) end,     desc = "Next Reference",     mode = { "n", "t" } },
+    { "[[",         function() Snacks.words.jump(-vim.v.count1) end,    desc = "Prev Reference",     mode = { "n", "t" } },
+    { "<c-_>",      function() Snacks.terminal() end,                   desc = "Toggle Terminal",    mode = { "n", "t" } },
+    -- grep
+    { "<leader>lg", function() Snacks.picker.grep() end,                desc = "Grep" },
+    -- search
+    { "<leader>sk", function() Snacks.picker.keymaps() end,             desc = "Keymaps" },
+    -- LSP
+    { "gd",         function() Snacks.picker.lsp_definitions() end,     desc = "Goto Definition" },
+    { "grr",        function() Snacks.picker.lsp_references() end,      nowait = true,               desc = "References" },
+    { "gI",         function() Snacks.picker.lsp_implementations() end, desc = "Goto Implementation" },
+    { "gci",        function() Snacks.picker.lsp_incoming_calls() end,  desc = "Calls Incoming" },
+    { "<leader>ss", function() Snacks.picker.lsp_symbols() end,         desc = "LSP Symbols" },
   }
 }
